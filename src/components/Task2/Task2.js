acc0aaf32de48f11c72b90e1bedd6a90046d2ff7
OlfiDell <bobr.rozrabiaka123@gmail.com>
2022-08-10 17:36:43 +0200

    task2 ready

--- src/components/Task2/Task2.js
+++ src/components/Task2/Task2.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 
 const Task2 = ({ start }) => {
   const [counter, setCounter] = useState(start);
+  const [inputValue, setInputValue] = useState(0);
 
   const incrementHandler = () => {
     setCounter((prevState) => prevState + 1);
@@ -13,14 +14,39 @@ const Task2 = ({ start }) => {
     setCounter((prevState) => prevState - 1);
   };
 
+  const inputValueHandler = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const changeValueHandler = () => {
+    setCounter(Number(inputValue));
+    setInputValue(0);
+  };
+
+  const resetHandler = () => {
+    setCounter(0);
+  };
+
   return (
     <div>
       <h2>Task2 counter</h2>
       <span data-testid="counter-element">{counter}</span>
       <div>
-        <button onClick={incrementHandler}>+</button>
-        <button onClick={decrementHandler}>-</button>
-        <button>reset</button>
+        <button onClick={incrementHandler} style={{margin: 4}}>+</button>
+        <button onClick={decrementHandler} style={{margin: 4}}>-</button>
+      </div>
+      <div>
+        <label htmlFor="changeValue">Change value</label>
+        <input
+          id="changeValue"
+          type="number"
+          value={inputValue}
+          onChange={inputValueHandler}
+        />
+        <button onClick={changeValueHandler}>Change</button>
+      </div>
+      <div>
+        <button onClick={resetHandler}>reset</button>
       </div>
     </div>
   );
--- src/components/Task2/Task2.spec.js
+++ src/components/Task2/Task2.spec.js
@@ -13,15 +13,28 @@ describe("task2 tests", () => {
   });
 
   it("should display start prop value", () => {
-    const startValue = -111;
+    const startValue = 10;
     renderComp(startValue);
     const counterVal = screen.getByTestId("counter-element");
 
     expect(counterVal).toHaveTextContent(startValue);
   });
 
+  it("should render increment and decrement button", () => {
+    renderComp();
+    const buttonPlus = screen.getByRole("button", {
+      name: /\+/i,
+    });
+    const buttonMinus = screen.getByRole("button", {
+      name: /-/i,
+    });
+
+    expect(buttonPlus).toBeInTheDocument();
+    expect(buttonMinus).toBeInTheDocument();
+  });
+
   it("should increment by 1", () => {
-    const startValue = 112;
+    const startValue = 10;
     renderComp(startValue);
     const counterVal = screen.getByTestId("counter-element");
     const buttonPlus = screen.getByRole("button", {
@@ -30,14 +43,49 @@ describe("task2 tests", () => {
 
     userEvent.click(buttonPlus);
     expect(counterVal).toHaveTextContent(startValue + 1);
-
-    // userEvent.click(buttonPlus);
-    // expect(counterVal).toHaveTextContent(2);
   });
-  test("renders three buttons", async () => {
+
+  it("should decremet by 1", () => {
+    const startValue = 10;
+    renderComp(startValue);
+    const counterVal = screen.getByTestId("counter-element");
+    const buttonMinus = screen.getByRole("button", {
+      name: /-/i,
+    });
+
+    userEvent.click(buttonMinus);
+    expect(counterVal).toHaveTextContent(startValue - 1);
+  });
+
+  it("should set value to 0", () => {
+    const startValue = 10;
+    renderComp(startValue);
+    const counterVal = screen.getByTestId("counter-element");
+    const buttonReset = screen.getByRole("button", {
+      name: /reset/i,
+    });
+
+    userEvent.click(buttonReset);
+    expect(counterVal).toHaveTextContent(0);
+  });
+
+  it("should set value to input value", () => {
+    const startValue = 10;
+    renderComp(startValue);
+    const counterVal = screen.getByTestId("counter-element");
+    const inputNode = screen.getByLabelText("Change value");
+    const buttonChange = screen.getByRole("button", {
+      name: /change/i,
+    });
+
+    userEvent.click(buttonChange);
+    expect(counterVal).toHaveTextContent(inputNode.value);
+  });
+
+  it("renders four buttons", async () => {
     const startValue = 112;
     renderComp(startValue);
     const items = await screen.findAllByRole("button");
-    expect(items).toHaveLength(3);
+    expect(items).toHaveLength(4);
   });
 });
